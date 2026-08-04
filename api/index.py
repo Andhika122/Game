68485dacbaefc533a1c3d8606bd63e8323ed6330
Andhika122 <kristaandhika1@gmail.com>
2026-08-04 15:45:13 +0700

    fixx18

--- api/index.py
+++ api/index.py
@@ -7,6 +7,8 @@ if ROOT not in sys.path:
 
 from app import app
 
+application = app
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port, debug=False)
